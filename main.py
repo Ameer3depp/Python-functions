3915acbf8c459ffc09f51864c03f955a25e0e145
ameer <ameer_depp@outlook.com>
2024-06-05 04:18:51 +0300

    adding mul, div functions

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 def sum(x, y):
   return x + y
 
+def mul(x, y):
+  return x * y
+
+def div(x, y):
+  return x / y
+
+
 def main():
-  pritn(sum(2, 4))
+  
+  print(sum(2, 4))
+  print(div(2, 4))
+  print(mul(2, 4))
 
 if __name__ == "__main__":
   main()
